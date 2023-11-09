ba3381334acf112d5614c137830675b896aa9ed0
arhaamwanii <arhamwani765@gmail.com>
2023-11-10 02:32:49 +0530

    ,

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     
+    // gets the menu links i.e stuff in side the hamburger
+    // gets the hamburger icon - outer clickable - 
+
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
+    //adds the open class to both of them
 
 }
