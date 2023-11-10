2e6b48bdd95f17f296665e2ac32213c6b8956a43
arhaamwanii <arhamwani765@gmail.com>
2023-11-10 14:26:07 +0530

    .

--- index.js
+++ index.js
@@ -14,26 +14,53 @@ function toggleMenu() {
 
 
 function updateTime() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+//   var seconds = now.getSeconds();
+
+  // Format the time to add leading zeros if the value is less than 10
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+//   seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  // Display the time in 24-hour format (you can modify this according to your preference)
+  var timeString = hours + ':' + minutes ;
+
+  // Update the content of the div with id "time"
+  document.getElementById("time").textContent = timeString;
+}
+
+// Call updateTime function initially to display the time immediately
+updateTime();
+
+// Call updateTime function every second to update the time every second
+setInterval(updateTime, 1000);
+
+
+
+
+// function updateTime() {
+//     var now = new Date();
+//     var hours = now.getHours();
+//     var minutes = now.getMinutes();
+//     var seconds = now.getSeconds();
   
-    // Format the time to add leading zeros if the value is less than 10
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+//     // Format the time to add leading zeros if the value is less than 10
+//     hours = hours < 10 ? '0' + hours : hours;
+//     minutes = minutes < 10 ? '0' + minutes : minutes;
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
   
-    // Display the time in 24-hour format (you can modify this according to your preference)
-    var timeString = hours + ':' + minutes + ':' + seconds;
+//     // Display the time in 24-hour format (you can modify this according to your preference)
+//     var timeString = hours + ':' + minutes + ':' + seconds;
   
-    // Update the content of the div with id "time"
-    document.getElementById("time").textContent = timeString;
-  }
+//     // Update the content of the div with id "time"
+//     document.getElementById("time").textContent = timeString;
+//   }
   
-  // Call updateTime function initially to display the time immediately
-  updateTime();
+//   // Call updateTime function initially to display the time immediately
+//   updateTime();
   
-  // Call updateTime function every second to update the time every second
-  setInterval(updateTime, 1000);
+//   // Call updateTime function every second to update the time every second
+//   setInterval(updateTime, 1000);
   
